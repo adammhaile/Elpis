96e3a61c9183fd8099aa57fde116f8b84aa590ff
elpis@adamhaile.net <elpis@adamhaile.net@03ac0622-d976-f640-46ab-04ada77f7b90>
2011-12-18 14:28:58 +0000

    Wasn't autoplaying next song.

--- Elpis/Properties/AssemblyInfo.cs
+++ Elpis/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.8.7.0")]
+[assembly: AssemblyVersion("0.8.8.0")]
 //[assembly: AssemblyFileVersion("0.0.0.0")]
