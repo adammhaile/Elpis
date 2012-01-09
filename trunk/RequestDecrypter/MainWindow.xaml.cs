7cba891027e40688322d0645f283f9b20afa58d6
elpis@adamhaile.net <elpis@adamhaile.net@03ac0622-d976-f640-46ab-04ada77f7b90>
2012-01-09 00:22:17 +0000

    Added "Add Variety to Station" option

---
+++ trunk/RequestDecrypter/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RequestDecrypter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            txtDecrypted.Text = PandoraSharp.PandoraCrypt.DecryptRPCRequest(txtEncrypted.Text);
+        }
+    }
+}
