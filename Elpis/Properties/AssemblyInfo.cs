5e0c8895a509647da32f0c018a8a67e09e3df6c3
elpis@adamhaile.net <elpis@adamhaile.net@03ac0622-d976-f640-46ab-04ada77f7b90>
2012-07-19 00:41:58 +0000

    Updated Version to 1.4.4

--- Elpis/Properties/AssemblyInfo.cs
+++ Elpis/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.4.3")]
+[assembly: AssemblyVersion("1.4.4")]
 //[assembly: AssemblyFileVersion("0.0.0.0")]
