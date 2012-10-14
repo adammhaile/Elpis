3092e351ef285b7fab3e5be75b558dbb0a4462b1
elpis@adamhaile.net <elpis@adamhaile.net@03ac0622-d976-f640-46ab-04ada77f7b90>
2012-10-14 14:41:41 +0000

    reworked command line handling... more work needed

---
+++ trunk/Elpis/CommandLineOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Elpis
+{
+    public class CommandLineOptions
+    {
+        public bool SkipTrack = false;
+        public bool TogglePlayPause = false;
+        public bool DoThumbsUp = false;
+        public bool DoThumbsDown = false;
+        public string StationToLoad = null;
+        public bool ShowHelp = false;
+        public string ConfigPath = null;
+    }
+}
