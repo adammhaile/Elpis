fcd9ac51e12774ac666c0c0fde387c594135de26
1119342 <zoryn4163@gmail.com>
2017-04-11 14:28:18 -0400

    Revert "ui updates"
    This reverts commit abe5c4782a6edf6eafef6de72b7bc98c95c086f7.

--- Elpis/Pages/PlaylistPage.xaml.cs
+++ Elpis/Pages/PlaylistPage.xaml.cs
@@ -25,7 +25,6 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using BassPlayer;
 using Elpis.Controls;
@@ -40,7 +39,6 @@ namespace Elpis
     /// </summary>
     public partial class PlaylistPage : UserControl
     {
-        public string CurSongArtUrl => _currSong.AlbumArtUrl;
         private readonly object _currSongLock = new object();
         private readonly Dictionary<Song, ImageButton[]> _feedbackMap;
         private readonly Player _player;
@@ -93,7 +91,7 @@ namespace Elpis
         }
 
         private void _player_PlaybackStateChanged(object sender, BassAudioEngine.PlayState oldState,
-            BassAudioEngine.PlayState newState)
+                                                  BassAudioEngine.PlayState newState)
         {
             lock (_currSongLock)
             {
@@ -105,21 +103,21 @@ namespace Elpis
         private void _player_StationLoaded(object sender, Station station)
         {
             this.BeginDispatch(() =>
-            {
-                txtStationName.Text = station.Name;
-                ShowWait(false);
-            });
+                                   {
+                                       txtStationName.Text = station.Name;
+                                       ShowWait(false);
+                                   });
         }
 
         private void _player_StationLoading(object sender, Station station)
         {
             this.BeginDispatch(() =>
-            {
-                if (IsLoaded)
-                {
-                    ShowWait(true);
-                }
-            });
+                                   {
+                                       if (IsLoaded)
+                                       {
+                                           ShowWait(true);
+                                       }
+                                   });
         }
 
         private void _player_PlaybackProgress(object sender, BassAudioEngine.Progress prog)
@@ -130,43 +128,43 @@ namespace Elpis
         private void UpdateProgress(BassAudioEngine.Progress prog)
         {
             this.BeginDispatch(() =>
-            {
-                lblCurrTime.Content = prog.ElapsedTime.ToString(@"mm\:ss");
-                lblRemainTime.Content = prog.RemainingTime.ToString(@"mm\:ss");
-                progPlayTime.Value = prog.Percent;
-            });
+                                   {
+                                       lblCurrTime.Content = prog.ElapsedTime.ToString(@"mm\:ss");
+                                       lblRemainTime.Content = prog.RemainingTime.ToString(@"mm\:ss");
+                                       progPlayTime.Value = prog.Percent;
+                                   });
         }
 
         private void _player_FeedbackUpdateEvent(object sender, Song song, bool success)
         {
             this.BeginDispatch(() =>
-            {
-                if (_feedbackMap.ContainsKey(song))
-                {
-                    //bit of a hack, but avoids putting INotify in lower level classes or making wrappers
-                    foreach (ImageButton button in _feedbackMap[song])
-                    {
-                        var spinner = button.FindParent<ContentSpinner>();
-                        BindingExpression bind =
-                            button.GetBindingExpression(ImageButton.IsActiveProperty);
-                        if (bind != null) bind.UpdateTarget();
-                        spinner.StopAnimation();
-                    }
-                    _feedbackMap.Remove(song);
+                                   {
+                                       if (_feedbackMap.ContainsKey(song))
+                                       {
+                                           //bit of a hack, but avoids putting INotify in lower level classes or making wrappers
+                                           foreach (ImageButton button in _feedbackMap[song])
+                                           {
+                                               var spinner = button.FindParent<ContentSpinner>();
+                                               BindingExpression bind =
+                                                   button.GetBindingExpression(ImageButton.IsActiveProperty);
+                                               if (bind != null) bind.UpdateTarget();
+                                               spinner.StopAnimation();
+                                           }
+                                           _feedbackMap.Remove(song);
 
-                    if (song.Banned && song == _player.CurrentSong) _player.Next();
-                }
-            });
+                                           if (song.Banned && song == _player.CurrentSong) _player.Next();
+                                       }
+                                   });
         }
 
         private void _player_LoadingNextSong(object sender)
         {
             UpdateProgress(new BassAudioEngine.Progress {ElapsedTime = new TimeSpan(0), TotalTime = new TimeSpan(0)});
             this.BeginDispatch(() =>
-            {
-                CurrentSong.Visibility = Visibility.Hidden;
-                WaitScreen.Visibility = Visibility.Visible;
-            });
+                                   {
+                                       CurrentSong.Visibility = Visibility.Hidden;
+                                       WaitScreen.Visibility = Visibility.Visible;
+                                   });
         }
 
         private void _player_PlayedSongRemoved(object sender, Song song)
@@ -196,21 +194,21 @@ namespace Elpis
         private Action AnimateListRemove(ContentControl item, bool last)
         {
             return () =>
-            {
-                Storyboard remSB;
-                if (last)
-                {
-                    remSB = ((Storyboard) Resources["ListBoxRemoveLast"]).Clone();
-                }
-                else
-                {
-                    remSB = ((Storyboard) Resources["ListBoxRemove"]).Clone();
-                    ((DoubleAnimation) remSB.Children[1]).From = (item).ActualHeight;
-                }
+                       {
+                           Storyboard remSB;
+                           if (last)
+                           {
+                               remSB = ((Storyboard) Resources["ListBoxRemoveLast"]).Clone();
+                           }
+                           else
+                           {
+                               remSB = ((Storyboard) Resources["ListBoxRemove"]).Clone();
+                               ((DoubleAnimation) remSB.Children[1]).From = (item).ActualHeight;
+                           }
 
-                remSB.Completed += ((o, e) => lstOldSongs.Items.Remove(item));
-                remSB.Begin(item);
-            };
+                           remSB.Completed += ((o, e) => lstOldSongs.Items.Remove(item));
+                           remSB.Begin(item);
+                       };
         }
 
         private void _player_PlayedSongAdded(object sender, Song song)
@@ -233,35 +231,26 @@ namespace Elpis
         private RoutedEventHandler AnimateListAdd(bool first)
         {
             return (o1, e1) =>
-            {
-                Storyboard addSB;
-                if (first)
-                {
-                    addSB = ((Storyboard) Resources["ListBoxAddFirst"]).Clone();
-                }
-                else
-                {
-                    addSB = ((Storyboard) Resources["ListBoxAdd"]).Clone();
-                    //((DoubleAnimation) addSB.Children[0]).To = 96;//((ContentControl)o1).ActualHeight;
-                }
-                addSB.Begin((ContentControl) o1);
+                       {
+                           Storyboard addSB;
+                           if (first)
+                           {
+                               addSB = ((Storyboard) Resources["ListBoxAddFirst"]).Clone();
+                           }
+                           else
+                           {
+                               addSB = ((Storyboard) Resources["ListBoxAdd"]).Clone();
+                               //((DoubleAnimation) addSB.Children[0]).To = 96;//((ContentControl)o1).ActualHeight;
+                           }
+                           addSB.Begin((ContentControl) o1);
 
-                var song = (ContentControl) o1;
-                song.Loaded -= (RoutedEventHandler) song.Tag;
-            };
+                           var song = (ContentControl) o1;
+                           song.Loaded -= (RoutedEventHandler) song.Tag;
+                       };
         }
 
         private void SetSong(Song song)
         {
-            try
-            {
-                MainWindow._mainWindow.mainBar.imgAlbumArt.Source = new BitmapImage(new Uri(song.AlbumArtUrl));
-            }
-            catch
-            {
-                
-            }
-
             CurrentSong.Content = song;
             CurrentSong.Visibility = Visibility.Visible;
             WaitScreen.Visibility = Visibility.Collapsed;
@@ -271,12 +260,9 @@ namespace Elpis
                 String[] stat = txtStationName.Text.Split('-');
                 if (stat[0].Equals("Quick Mix"))
                 {
-                    txtStationName.Text = stat[0] + "-" + song.Station.Name + " - " + song.Artist + " - " + song.SongTitle;
+                    txtStationName.Text = stat[0]+"-"+song.Station.Name;
                 }
             });
-
-            txtStationName.Text = song.Station.Name + " - " + song.Artist + " - " + song.SongTitle;
-            MainWindow._mainWindow.Title = "Elpis | Pandora";
         }
 
         private void _player_SongStarted(object sender, Song song)
@@ -300,13 +286,13 @@ namespace Elpis
 
         private void ThumbDownHandle(ImageButton button)
         {
-            var song = (Song) ((button).FindParentByName<Grid>("SongItem")).DataContext;
+            var song = (Song)((button).FindParentByName<Grid>("SongItem")).DataContext;
             var spinner = button.FindParent<ContentSpinner>();
             if (_feedbackMap.ContainsKey(song)) return;
 
             var otherButton =
                 spinner.FindSiblingByName<ContentSpinner>("SpinUp").FindChildByName<ImageButton>("btnThumbUp");
-            _feedbackMap.Add(song, new[] {button, otherButton});
+            _feedbackMap.Add(song, new[] { button, otherButton });
 
             if (song.Banned)
                 _player.SongDeleteFeedback(song);
@@ -318,7 +304,7 @@ namespace Elpis
 
         private void btnThumbDown_Click(object sender, RoutedEventArgs e)
         {
-            ThumbDownHandle((ImageButton) sender);
+            ThumbDownHandle((ImageButton)sender);
         }
 
         public void ThumbUpCurrent()
@@ -329,13 +315,13 @@ namespace Elpis
 
         private void ThumbUpHandle(ImageButton button)
         {
-            var song = (Song) ((button).FindParentByName<Grid>("SongItem")).DataContext;
+            var song = (Song)((button).FindParentByName<Grid>("SongItem")).DataContext;
             var spinner = button.FindParent<ContentSpinner>();
             if (_feedbackMap.ContainsKey(song)) return;
 
             var otherButton =
                 spinner.FindSiblingByName<ContentSpinner>("SpinDown").FindChildByName<ImageButton>("btnThumbDown");
-            _feedbackMap.Add(song, new[] {button, otherButton});
+            _feedbackMap.Add(song, new[] { button, otherButton });
             if (song.Loved)
                 _player.SongDeleteFeedback(song);
             else
@@ -347,7 +333,7 @@ namespace Elpis
 
         private void btnThumbUp_Click(object sender, RoutedEventArgs e)
         {
-            ThumbUpHandle((ImageButton) sender);
+            ThumbUpHandle((ImageButton)sender);
         }
 
         private Song GetItemSong(object sender)
@@ -460,7 +446,7 @@ namespace Elpis
                 }
                 else
                 {
-                    if (_currMenuSong.AmazonAlbumUrl != null)
+                    if(_currMenuSong.AmazonAlbumUrl != null)
                     {
                         string url = _currMenuSong.AmazonAlbumUrl;
 
@@ -487,7 +473,7 @@ namespace Elpis
 
         private void seekToPosition(object sender, MouseEventArgs e)
         {
-            int percentClicked = (int) Math.Round(e.GetPosition((ProgressBar) sender).X/this.progPlayTime.ActualWidth*100);
+            int percentClicked = (int)Math.Round(e.GetPosition((ProgressBar)sender).X / this.progPlayTime.ActualWidth * 100);
             //Util.Log.O("Bar percentage, numerator, denominator: {0} {1} {2}", new object[] {percentClicked, e.GetPosition((ProgressBar)sender).X, this.progPlayTime.ActualWidth});
             _player.SeekToTime(percentClicked);
         }
