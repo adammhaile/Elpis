11c219ae1d951ac2360c04a8d212bf11fd9b0c09
Trevor Lund <tlund@ymail.com>
2017-06-08 21:31:14 -0500

    Prepping for 1.6.0

--- Elpis/Properties/AssemblyInfo.cs
+++ Elpis/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.5.3")]
+[assembly: AssemblyVersion("1.6.0")]
 //[assembly: AssemblyFileVersion("0.0.0.0")]
